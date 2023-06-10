d2e744a3938be5d800b38559909e2be59dca5684
Robert Kimura <chknmanrob1904@gmail.com>
2023-06-09 22:04:53 -0700

    turning to async

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use clap::{builder::Str, Arg, ArgAction, ArgMatches, Command, Subcommand};
+use futures::future::join;
 use rusqlite::{Connection, Result};
+use scryfall::card::Card;
+use tokio::runtime::Runtime;
 
 #[derive(Debug)]
-struct Card {
+struct SCard {
     name: String,
     set_code: String,
     amount: i32,
@@ -9,31 +13,94 @@ struct Card {
     formats: Vec<String>,
 }
 
-fn main() -> Result<()> {
-    let conn = Connection::open("./card_database")?;
-
-    let mut db_check = conn.prepare("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='card'")?;
+fn check_for_table(conn: &Connection) -> Result<()> {
+    let mut db_check =
+        conn.prepare("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='card'")?;
     let mut check = db_check.query([])?;
 
     if let Some(row) = check.next()? {
-        let x:i32 = row.get(0)?;
-        if x == 0{
-        conn.execute(
-            "CREATE TABLE card (
+        let x: i32 = row.get(0)?;
+        if x == 0 {
+            conn.execute(
+                "CREATE TABLE card (
             name TEXT NOT NULL PRIMARY KEY,
             set_code NOT NULL,
             amount INTEGAR NOT NULL,
             prince FLOAT,
             formats BLOB
             )",
-         (),
-        )?;
-        //println!("created table");
+                (),
+            )?;
+            //println!("created table");
         }
     }
-    //println!("done");
+    Ok(())
+}
 
-    
+fn look_up_card(runtime: &Runtime, card_name: &str, amount: i32) -> scryfall::Card{
+    println!("{:?} {:?}", card_name, &amount);
+    let card: Card;
+    let result = runtime.block_on(Card::named_fuzzy(card_name));
+    match result {
+        Ok(c) => card = c,
+        Err(e) => panic!("{}", format!("{:?}", e)),
+    }
+    card
+}
+
+fn main() -> Result<()> {
+    let conn = Connection::open("./card_database")?;
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    check_for_table(&conn)?;
+
+    let prog = Command::new("card-storage")
+        // TODO: Add flag file to to batch work
+        .arg(
+            Arg::new("file")
+                .short('f')
+                .action(ArgAction::SetTrue)
+                .global(true),
+        )
+        .subcommand(
+            Command::new("add").args([
+                Arg::new("card").help("card name"),
+                Arg::new("amount")
+                    .default_value("1")
+                    .help("number of cards default is 1"),
+            ]),
+        )
+        .subcommand(
+            Command::new("remove").args([
+                Arg::new("card").help("card namem"),
+                Arg::new("amount")
+                    .default_value("0")
+                    .help("amount of cards to remove"),
+            ]),
+        )
+        .get_matches();
+
+    match prog.subcommand() {
+        Some(("add", args)) => {
+            if !args.get_flag("file") {
+                let card_name = args.get_one::<String>("card").unwrap();
+                let amount = args.get_one::<String>("amount").unwrap().parse::<i32>().unwrap();
+                let card = look_up_card(&runtime, card_name, amount);
+                println!("{}", card.prices.usd.unwrap());
+            }
+        }
+        Some(("remove", args)) => {
+            println!(
+                "{:?} {:?}",
+                args.get_one::<String>("card"),
+                args.get_one::<String>("amount").unwrap().parse::<i32>()
+            )
+        }
+        _ => println!("No command inputed"),
+    }
 
     // conn.execute(
     //     "CREATE TABLE person (
